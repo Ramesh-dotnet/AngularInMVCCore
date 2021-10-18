54e442fa081051aae075dd50125e2fe77a19dbb7
home <home@DESKTOP-RJEQKGD>
2021-10-18 21:22:18 +0530

    Add project files.

---
+++ Controllers/TestAngular.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularInMVCCore.Controllers
+{
+    public class TestAngular : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
